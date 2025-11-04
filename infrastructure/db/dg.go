21837922aa92a23d69dce6ef13a320096b9f151f
Jordany <j2d6.pro@gmail.com>
2025-11-04 10:31:42 +0300

    credentials for render

--- infrastructure/db/dg.go
+++ infrastructure/db/dg.go
@@ -7,7 +7,7 @@ import (
 
 
 func CreateNewPgxConnexion() (*pgx.Conn, error) {
-	conn, err := pgx.Connect(context.Background(), 	"postgres://postgres:BoissonXXLenergy261001..@localhost:5432/reny_event")
+	conn, err := pgx.Connect(context.Background(), 	"postgres://reny_event:vepC19IWOqDgy68Va3p6tdtDO1WW1iYV@dpg-d44qlgbipnbc73aps8i0-a:5432/reny_event")
 	if err != nil {
 		return nil, err
 	}
