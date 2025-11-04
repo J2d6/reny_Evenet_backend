1bce84ea548d7b31a9e8179a4ac789e98da66711
Jordany <131016725+J2d6@users.noreply.github.com>
2025-11-04 12:07:55 +0300

    Update dg.go

--- infrastructure/db/dg.go
+++ infrastructure/db/dg.go
@@ -7,7 +7,7 @@ import (
 
 
 func CreateNewPgxConnexion() (*pgx.Conn, error) {
-	conn, err := pgx.Connect(context.Background(), 	"postgresql://postgres:gF7dYGWDK9tOUzCN@db.bbfsckuzadzzsdymgzmj.supabase.co:5432/postgres")
+	conn, err := pgx.Connect(context.Background(),  	"postgresql://postgres:gF7dYGWDK9tOUzCN@db.bbfsckuzadzzsdymgzmj.supabase.co:5432/postgres")
 	if err != nil {
 		return nil, err
 	}
